cf9ed89055d4f41c6a631d98a8da1fe015474a8e
Phytoo <52602080+PhytooGod@users.noreply.github.com>
2022-06-25 12:39:51 +0500

    Fourth commit

---
+++ Numberstestbackend/Numberstestbackend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Numberstestbackend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Numberstestbackend.settings')
+
+application = get_asgi_application()
